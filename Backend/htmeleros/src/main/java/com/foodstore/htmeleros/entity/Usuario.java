5d4ccbb1cb3c8f34e696fd0e11d4031aea18de3a
GordoMatti <matinicoher@gmail.com>
2025-11-07 20:40:30 -0300

    actualizacion
    usuario, pedido, detalle pedido y una parte de excepción.

---
+++ Backend/htmeleros/src/main/java/com/foodstore/htmeleros/entity/Usuario.java
@@ -0,0 +1,31 @@
+package com.foodstore.htmeleros.entity;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String email;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Pedido> pedidos;
+}
